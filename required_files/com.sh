0a24675c1cdb43077c18341f2718f07a462d9506
Sourav <Sourav@DESKTOP-L4F7O9H>
2018-07-09 22:42:48 +0530

    changes in com.sh

--- required_files/com.sh
+++ required_files/com.sh
@@ -1,12 +1,33 @@
 user_input_dir=$1
 user_output_directory=$2
+user_jar=$3
+if [ "$user_jar" = "" ]
+then
+    user_jar="productprob.jar"
+else
+    echo "your selected jar file : $3"
+fi
+if [ "$user_output_directory" = "" ]
+then
+    user_output_directory="OutputFolder"
+else
+    echo "your selected output folder will be : $2"
+fi
+if [ "$user_input_dir" = "" ]
+then
+    user_input_dir="MBA.txt"
+else
+    echo "your selected inputFile is : $1"
+fi
+echo "files given: 1)$user_input_dir 2)$user_output_directory 3)$user_jar"
+
 hadoop fs -rm -r /user/indir
 hadoop fs -mkdir /user/indir
 hadoop fs -put $user_input_dir /user/indir
 hadoop fs -rm -r /user/outdir*
-hadoop jar productprob.jar org.ProductDriver /user/indir /user/outdir /user/outdir1 /user/outdir2 /user/outdir3 /user/outdir4
-rm -r $2
-mkdir $2
-hadoop fs -get /user/outdir4/part-r-00000 $2
-mv $2/part-r-00000 $2/output.txt
+hadoop jar $user_jar org.ProductDriver /user/indir /user/outdir /user/outdir1 /user/outdir2 /user/outdir3 /user/outdir4
+rm -r $user_output_directory
+mkdir $user_output_directory
+hadoop fs -get /user/outdir4/part-r-00000 $user_output_directory
+mv $user_output_directory/part-r-00000 $user_output_directory/output.txt
 echo "Output File created at :$2/output.txt"
